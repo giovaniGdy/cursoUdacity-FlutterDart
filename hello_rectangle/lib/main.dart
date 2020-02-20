2352517c1ce060c7933d4cad22d605fee50b65a3
giovaniGdy <giovani.godoy@sou.unifeob.edu.br>
2020-02-20 15:25:11 -0300

    Arquivos das Aulas

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.red,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            "Hello",
+            style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 void main() {
   print('Hello');
   runApp(
-    Container(
-      color: Colors.green,
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Rectangle"),
+        ),
+        body: HelloRectangle(),
+      ),
     ),
   );
 }
